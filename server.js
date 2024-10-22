98ddb2418273187b1aa2129b09ce657add6669c8
Asim-Cnetric <asim@cnetric.com>
2024-10-23 00:11:59 +0530

    Updated server with cors
    updated server.js with cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const corsMiddleware = require('./middleware/cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.use('/api/uc/users', require('./routes/authRoutes'));
